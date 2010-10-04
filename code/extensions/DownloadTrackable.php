62f78dacf5d00293a8d970dadbdf8bba4b91eaf8
nyeholt <nyeholt@gmail.com>
2010-10-04 18:02:59 +1100

    Only track downlaods for the raw file, not assetfield previews

--- code/extensions/DownloadTrackable.php
+++ code/extensions/DownloadTrackable.php
@@ -30,14 +30,32 @@ class DownloadTrackable extends DataObjectDecorator {
 				'By' => '".$Downloader()."'
 			));
 
+			$total = DB::query(sprintf(
+				"SELECT COUNT(*) FROM \"%s\" WHERE \"%s\" = %d",
+				'FileDownloadRecord',
+				'FileID',
+				(int) $this->owner->ID
+			))->value();
+
+			$tf = new TextField('DownloadTotal', _t('DownloadTrackable.TOTAL', 'Total'), $total);
+			$tf = $tf->performReadonlyTransformation();
+			$fields->addFieldToTab('BottomRoot.'._t('DownloadTrackable.DOWNLOADTRACKINGTAB', 'Downloads'), $tf);
 			$fields->addFieldToTab('BottomRoot.'._t('DownloadTrackable.DOWNLOADTRACKINGTAB', 'Downloads'), $tableField);
 		}
 	}
 
 	public function onAccessGranted() {
+		// check to make sure this is the actual file and not just a rendition... if that matters?
+		$o = $this->owner;
+		
 		$downloadRecord = new FileDownloadRecord();
 		// store the full filename for now
 		$url = array_key_exists('url', $_GET) ? $_GET['url'] : $_SERVER['REQUEST_URI'];
+		// check if it's a resampled file
+		if (strpos($url, '_resampled/AssetLibraryPreview')) {
+			// ignore it
+			return;
+		}
 		$file_path = Director::makeRelative($url);
 		$downloadRecord->Filename = $file_path ? $file_path : $this->owner->getFilename();
 		$downloadRecord->FileID = $this->owner->ID;
